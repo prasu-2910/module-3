20726be4c02aa7df8d565671ce68bd02a4ae288d
prasu-2910 <prasuaruna29@gmail.com>
2025-12-21 18:10:24 +0530

    first commit

---
+++ OneDrive/Desktop/evaluation/Q1/code/src/App.jsx
@@ -0,0 +1,11 @@
+//import Student from "./Student"
+//function App(){
+  //return(
+
+import MyComponent from "./mycomponent";
+
+  function App(){
+    return(<MyComponent/>)
+
+  }
+  export default App;
